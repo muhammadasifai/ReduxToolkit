0c3aa37cb4bdc0ae2cf0556b302776ccf7bcaec9
muhammadasifai <muhammadasifdotai@gmail.com>
2024-04-25 20:03:12 +0500

    first commit

---
+++ src/routes/AppNavigation.js
@@ -0,0 +1,37 @@
+import {StyleSheet} from 'react-native';
+import React from 'react';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import Login from '../screens/Login';
+import {NavigationContainer} from '@react-navigation/native';
+import TabNavigation from './TabNavigation';
+import CounterScreen from '../screens/CounterScreen';
+import CartScreen from '../screens/CartScreen';
+import SingleProduct from '../screens/SingleProduct';
+
+const Stack = createNativeStackNavigator();
+
+const AppNavigation = () => {
+  const userData = false;
+  return (
+    <NavigationContainer>
+      <Stack.Navigator screenOptions={{headerShown: false}}>
+        {userData ? (
+          <Stack.Group>
+            <Stack.Screen name="root" component={TabNavigation} />
+            <Stack.Screen name="SingleProduct" component={SingleProduct} />
+            <Stack.Screen name="Cart" component={CartScreen} />
+          </Stack.Group>
+        ) : (
+          <Stack.Group>
+            <Stack.Screen name="CounterScreen" component={CounterScreen} />
+            <Stack.Screen name="Login" component={Login} />
+          </Stack.Group>
+        )}
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default AppNavigation;
+
+const styles = StyleSheet.create({});
